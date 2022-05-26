dfe96b0133d9f2fbc16f40fbeb26c34fe5ecb7dc
xiejon <jxie77@gmail.com>
2022-05-26 20:27:26 +0100

    added function to create clock svg using js

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,18 @@
 function generateContactPage(element) {
     const container = document.createElement('div');
-    const title = document.createElement('p');
+    const address = document.createElement('p');
+    const phone = document.createElement('p');
 
     container.classList.add('contact');
 
-    title.textContent = 'Contact Us';
+    address.textContent = 'Greenwich Village, New York, NY 10012, USA';
+    phone.textContent = '(212) 893-9938';
 
     element.appendChild(container);
-    container.appendChild(title);
+    container.appendChild(address);
+    container.appendChild(phone);
     embedMap(container);
+    renderClockSVG(container);
 
     // hide until called
     container.style.display = 'none';
@@ -24,6 +28,23 @@ function embedMap(node) {
     iframe.setAttribute('referrerpolicy', 'no-referrer-when-downgrade');
 
     return node.appendChild(iframe);
+}
+
+function renderClockSVG(node) {
+    const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const iconPath1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const iconPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+    iconSvg.setAttribute('width', '24');
+    iconSvg.setAttribute('height', '24');
+    iconSvg.setAttribute('viewbox', '0 0 24 24');
+    iconPath1.setAttribute('d', "M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm0,22A10,10,0,1,1,22,12,10.011,10.011,0,0,1,12,22Z");
+    iconPath2.setAttribute('d', "M12,6a1,1,0,0,0-1,1v4.325L7.629,13.437a1,1,0,0,0,1.062,1.7l3.84-2.4A1,1,0,0,0,13,11.879V7A1,1,0,0,0,12,6Z");
+
+    iconSvg.appendChild(iconPath1);
+    iconSvg.appendChild(iconPath2);
+
+    return node.appendChild(iconSvg);
 }
 
 module.exports = {
