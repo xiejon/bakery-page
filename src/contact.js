92f75da666e6776a0d59a5ee53a3e6fb7f870548
xiejon <jxie77@gmail.com>
2022-05-26 21:10:20 +0100

    wrote function to create svg, created 3 icons

--- src/contact.js
+++ src/contact.js
@@ -12,8 +12,9 @@ function generateContactPage(element) {
     container.appendChild(address);
     container.appendChild(phone);
     embedMap(container);
-
-    clockSVG(container);
+    clockSvg(container);
+    markerSvg(container);
+    phoneSvg(container);
 
     // hide until called
     container.style.display = 'none';
@@ -31,26 +32,39 @@ function embedMap(node) {
     return node.appendChild(iframe);
 }
 
-const clockSVG = (node) => {
-    renderSVG(node, 
+const clockSvg = (node) => {
+    renderSvg(node, 
         "M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm0,22A10,10,0,1,1,22,12,10.011,10.011,0,0,1,12,22Z",
         "M12,6a1,1,0,0,0-1,1v4.325L7.629,13.437a1,1,0,0,0,1.062,1.7l3.84-2.4A1,1,0,0,0,13,11.879V7A1,1,0,0,0,12,6Z");
 }
 
+const markerSvg = (node) => {
+    renderSvg(node, 
+        "M12,6a4,4,0,1,0,4,4A4,4,0,0,0,12,6Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,12,12Z",
+        "M12,24a5.271,5.271,0,0,1-4.311-2.2c-3.811-5.257-5.744-9.209-5.744-11.747a10.055,10.055,0,0,1,20.11,0c0,2.538-1.933,6.49-5.744,11.747A5.271,5.271,0,0,1,12,24ZM12,2.181a7.883,7.883,0,0,0-7.874,7.874c0,2.01,1.893,5.727,5.329,10.466a3.145,3.145,0,0,0,5.09,0c3.436-4.739,5.329-8.456,5.329-10.466A7.883,7.883,0,0,0,12,2.181Z");
+}
 
-function renderSVG(node, d1, d2) {
+const phoneSvg = (node) => {
+    renderSvg(node, 
+        "M13,1a1,1,0,0,1,1-1A10.011,10.011,0,0,1,24,10a1,1,0,0,1-2,0,8.009,8.009,0,0,0-8-8A1,1,0,0,1,13,1Zm1,5a4,4,0,0,1,4,4,1,1,0,0,0,2,0,6.006,6.006,0,0,0-6-6,1,1,0,0,0,0,2Zm9.093,10.739a3.1,3.1,0,0,1,0,4.378l-.91,1.049c-8.19,7.841-28.12-12.084-20.4-20.3l1.15-1A3.081,3.081,0,0,1,7.26.906c.031.031,1.884,2.438,1.884,2.438a3.1,3.1,0,0,1-.007,4.282L7.979,9.082a12.781,12.781,0,0,0,6.931,6.945l1.465-1.165a3.1,3.1,0,0,1,4.281-.006S23.062,16.708,23.093,16.739Zm-1.376,1.454s-2.393-1.841-2.424-1.872a1.1,1.1,0,0,0-1.549,0c-.027.028-2.044,1.635-2.044,1.635a1,1,0,0,1-.979.152A15.009,15.009,0,0,1,5.9,9.3a1,1,0,0,1,.145-1S7.652,6.282,7.679,6.256a1.1,1.1,0,0,0,0-1.549c-.031-.03-1.872-2.425-1.872-2.425a1.1,1.1,0,0,0-1.51.039l-1.15,1C-2.495,10.105,14.776,26.418,20.721,20.8l.911-1.05A1.121,1.121,0,0,0,21.717,18.193Z");
+}
+
+function renderSvg(node, d1, d2) {
     const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     const iconPath1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    const iconPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
     iconSvg.setAttribute('width', '24');
     iconSvg.setAttribute('height', '24');
-    iconSvg.setAttribute('viewbox', '0 0 24 24');
+    iconSvg.setAttribute('viewbox', `0 0 24 24`);
     iconPath1.setAttribute('d', d1);
-    iconPath2.setAttribute('d', d2);
 
     iconSvg.appendChild(iconPath1);
-    iconSvg.appendChild(iconPath2);
+
+    if (d2) {
+        const iconPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        iconPath2.setAttribute('d', d2);
+        iconSvg.appendChild(iconPath2);
+    }
 
     return node.appendChild(iconSvg);
 }
